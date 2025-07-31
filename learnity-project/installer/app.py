614007d6b8f252bbd7a8fd2a34e8428b1b493898
Learnity <aiplanning1@gmail.com>
2025-07-31 12:26:18 +0700

    Update app.py

--- learnity-project/installer/app.py
+++ learnity-project/installer/app.py
@@ -10,18 +10,15 @@ try:
 except ImportError:
     os.system("pip install pywebview")
 
-# Fungsi utilitas untuk install dependensi
 def install_dependency(pkg_name, module_name=None):
     if module_name is None:
         module_name = pkg_name
-
-    # Cek apakah modul sudah tersedia
     if importlib.util.find_spec(module_name) is not None:
         return f"✅ {pkg_name} sudah terinstall."
     else:
+        os.system(f"pip install {module_name}")
         return f"❌ {pkg_name} belum terinstall, menginstall.."
 
-# API yang dipanggil dari frontend
 class API:
     def check_dependicies(self):
         time.sleep(5.0)
@@ -34,14 +31,12 @@ class API:
         messages.append(install_dependency("streamlit"))
         messages.append(install_dependency("Pillow", "PIL"))
         messages.append(install_dependency("pywebview", "webview"))
-        messages.append(install_dependency("PyWebviewss"))
         messages.append(install_dependency("cv2"))
         messages.append(install_dependency("mediapipe"))
         messages.append(install_dependency("pygetwindow"))
         messages.append(install_dependency("pyautogui"))
         return messages
 
-# Jalankan PyWebView
 html_path = os.path.abspath("index.html")
 api = API()
 window = webview.create_window("Learnity Setup",
